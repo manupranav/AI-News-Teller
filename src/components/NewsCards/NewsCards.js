68df70882463b9550417ba17289edb46ada04ce0
manupranav <62414791+manupranav@users.noreply.github.com>
2022-08-22 18:03:24 -0700

    added infocards

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -4,6 +4,28 @@ import { Grid, Grow, Typography } from "@material-ui/core";
 import useStyles from "./styles.js";
 import NewsCard from "../NewsCard/NewsCard";
 
+const infoCards = [
+  { color: "#00838f", title: "Latest News", text: "Give me the latest news" },
+  {
+    color: "#1565c0",
+    title: "News by Categories",
+    info: "Business, Entertainment, General, Health, Science, Sports, Technology",
+    text: "Give me the latest Technology news",
+  },
+  {
+    color: "#4527a0",
+    title: "News by Terms",
+    info: "Bitcoin, PlayStation 5, Smartphones, Donald Trump...",
+    text: "What's up with PlayStation 5",
+  },
+  {
+    color: "#283593",
+    title: "News by Sources",
+    info: "CNN, Wired, BBC News, Time, IGN, Buzzfeed, ABC News...",
+    text: "Give me the news from CNN",
+  },
+];
+
 const NewsCards = ({ articles }) => {
   const classes = useStyles();
   return (
@@ -16,7 +38,7 @@ const NewsCards = ({ articles }) => {
       >
         {articles.map((article, i) => (
           <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: "flex" }}>
-            <NewsCard />
+            <NewsCard article={article} i={i} />
           </Grid>
         ))}
       </Grid>
