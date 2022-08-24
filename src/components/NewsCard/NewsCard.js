ebdee90ab00cd04d0e336ee813849bf3e555ecce
manupranav <62414791+manupranav@users.noreply.github.com>
2022-08-23 19:27:06 -0700

    added 3 more alan command

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect, createRef } from "react";
-
+import React from "react";
 import {
   Card,
   CardActions,
@@ -8,76 +7,10 @@ import {
   CardMedia,
   Button,
   Typography,
-} from "@material-ui/core";
+} from "@material-ui/core/CardActions";
 
-import classNames from "classnames";
-import useStyles from "./styles.js";
-
-const NewsCard = ({
-  article: { description, publishedAt, source, title, url, urlToImage },
-  activeArticle,
-  i,
-}) => {
-  const classes = useStyles();
-  const [elRefs, setElRefs] = useState([]);
-  const scrollToRef = (ref) => window.scroll(0, ref.current.offsetTop - 50);
-
-  useEffect(() => {
-    window.scroll(0, 0);
-
-    setElRefs((refs) =>
-      Array(20)
-        .fill()
-        .map((_, j) => refs[j] || createRef())
-    );
-  }, []);
-
-  useEffect(() => {
-    if (i === activeArticle && elRefs[activeArticle]) {
-      scrollToRef(elRefs[activeArticle]);
-    }
-  }, [i, activeArticle, elRefs]);
-
-  return (
-    <Card
-      ref={elRefs[i]}
-      className={activeArticle === i ? classes.activeCard : classes.card}
-    >
-      <CardActionArea href={url} target="_blank">
-        <CardMedia
-          className={classes.media}
-          image={
-            urlToImage ||
-            "https://pixabay.com/illustrations/news-headlines-newsletter-636978/"
-          }
-        />
-        <div className={classes.details}>
-          <Typography varient="body2" color="textSecondary" component="h2">
-            {new Date(publishedAt).toDateString()}
-          </Typography>
-          <Typography varient="body2" color="textSecondary" component="h2">
-            {source.name}
-          </Typography>
-        </div>
-        <Typography className={classes.title} gutterBottom varient="h5">
-          {title}
-        </Typography>
-        <CardContent>
-          <Typography varient="body2" color="textSecondary" component="p">
-            {description}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions className={classes.cardActions}>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-        <Typography varient="h5" color="textSecondary">
-          {i + 1}
-        </Typography>
-      </CardActions>
-    </Card>
-  );
+const NewsCard = () => {
+  return;
 };
 
 export default NewsCard;
--- src/components/NewsCards/styles.js
+++ src/components/NewsCards/styles.js
@@ -6,22 +6,4 @@ export default makeStyles({
     width: "100%",
     margin: 0,
   },
-
-  card: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
-    alignItems: "center",
-    width: "100%",
-    height: "45vh",
-    padding: "10%",
-    borderRadius: 10,
-    color: "white",
-  },
-
-  infoCard: {
-    display: "flex",
-    flexDirection: "column",
-    textAlign: "center",
-  },
 });
