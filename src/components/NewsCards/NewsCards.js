ba78803210fa7a5ca35a51d86183e78207fe5137
manupranav <62414791+manupranav@users.noreply.github.com>
2022-08-23 19:22:00 -0700

    added 3 more commands

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -5,7 +5,7 @@ import useStyles from "./styles.js";
 import NewsCard from "../NewsCard/NewsCard";
 
 const infoCards = [
-  { color: "#00838f", title: "Latest News", text: "Give me the latest news" },
+  { color: "#00838f", title: "Latest News", text: "Show me the latest news" },
   {
     color: "#1565c0",
     title: "News by Categories",
@@ -15,19 +15,61 @@ const infoCards = [
   {
     color: "#4527a0",
     title: "News by Terms",
-    info: "Bitcoin, PlayStation 5, Smartphones, Donald Trump...",
-    text: "What's up with PlayStation 5",
+    info: "Bitcoin, PlayStation 5, Smartphones, Joe Biden...",
+    text: "What's up with Bitcoin",
   },
   {
     color: "#283593",
     title: "News by Sources",
-    info: "CNN, Wired, BBC News, Time, IGN, Buzzfeed, ABC News...",
+    info: "CNN, Wired, Techcrunch, Time, IGN, Buzzfeed, ABC News...",
     text: "Give me the news from CNN",
   },
 ];
 
-const NewsCards = ({ articles }) => {
+const NewsCards = ({ articles, activeArticle }) => {
   const classes = useStyles();
+
+  if (!articles.length) {
+    return (
+      <Grow in>
+        <Grid
+          className={classes.container}
+          container
+          alignItems="stretch"
+          spacing={3}
+        >
+          {infoCards.map((infoCard) => (
+            <Grid
+              item
+              xs={12}
+              sm={6}
+              md={4}
+              lg={3}
+              className={classes.infoCard}
+            >
+              <div
+                className={classes.card}
+                style={{ backgroundColor: infoCard.color }}
+              >
+                <Typography variant="h5">{infoCard.title}</Typography>
+                {infoCard.info ? (
+                  <Typography varient="h6">
+                    <strong>{infoCard.title.split(" ")[2]}:</strong>
+                    <br />
+                    {infoCard.info}
+                  </Typography>
+                ) : null}
+                <Typography varient="h5">
+                  Try saying: <br />
+                  <em>{infoCard.text}</em>
+                </Typography>
+              </div>
+            </Grid>
+          ))}
+        </Grid>
+      </Grow>
+    );
+  }
   return (
     <Grow in>
       <Grid
@@ -38,7 +80,7 @@ const NewsCards = ({ articles }) => {
       >
         {articles.map((article, i) => (
           <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: "flex" }}>
-            <NewsCard article={article} i={i} />
+            <NewsCard article={article} activeArticle={activeArticle} i={i} />
           </Grid>
         ))}
       </Grid>
